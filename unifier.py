c7f9cdf56d122a131fcd07c539760abba19a9958
Aviv <54484588+avivper@users.noreply.github.com>
2024-03-11 18:34:17 +0200

    Add files via upload

--- unifier.py
+++ unifier.py
@@ -1,3 +1,7 @@
+import os.path
+from time import sleep
+
+import PyPDF2
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QFont
 
@@ -8,16 +12,23 @@ class Unifier(QWidget):
 
         self.layout = QVBoxLayout(self)
 
-        # Input
-        self.input = QLabel("Select the PDF file: ")
-        self.input_text = QLineEdit()  # Textfield of the targeted PDF
-        self.input_file = QPushButton("Browse")
+        # Create 2 LineEdit(), in Parallel to input 2 path and will unify them
+
+        # 2 Input in parallel
+        self.input = QLabel("Select two PDF files to unify ")
+        self.input_1 = QLineEdit()
+        self.input_2 = QLineEdit()
+        self.browse = QPushButton("Browse")
+
+        # Output
+        self.output = QLabel("Enter the name for your new PDF file:\n(Without .pdf)")
+        self.output_text = QLineEdit()  # Textfield of output new file
 
         self.create = QPushButton("Unify")
 
         self.Widgets = [
-            self.input, self.input_text, self.input_file,
-            self.create
+            self.input, self.input_1, self.input_2, self.browse,
+            self.output, self.output_text, self.create
         ]
 
         self.init()
@@ -32,4 +43,69 @@ class Unifier(QWidget):
                 font = QFont("Arial", 11)
                 widget.setFont(font)
 
-            self.setLayout(self.layout)
+        self.browse.clicked.connect(self.choose)
+        self.setLayout(self.layout)
+
+    def choose(self):
+        file_path, _ = QFileDialog.getOpenFileName(
+            self, "Select a PDF file", "", "PDF Files (*.pdf)"
+        )
+
+        if file_path:
+            self.input_path(file_path)
+
+    def input_path(self, file_path):
+        option_dialog = RadioInput()
+        selected_option = option_dialog.get_option()
+        parse_option = option_dialog.parse_input()
+
+        if selected_option:
+            print(f"{selected_option}, \n" + file_path)
+            self.input_1.setText(file_path)
+
+        if parse_option:
+            print(f"{parse_option}")
+
+
+class RadioInput(QDialog):
+    def __init__(self, parent=None):
+        super(RadioInput, self).__init__(parent)
+
+        self.setWindowTitle("Input")
+
+        self.layout = QVBoxLayout(self)
+        self.form_layout = QFormLayout()
+
+        self.input_1 = QRadioButton("File 1")
+        self.input_2 = QRadioButton("File 2")
+
+        self.form_layout.addRow("Select an option: ", self.input_1)
+        self.form_layout.addRow("", self.input_2)
+
+        self.buttons_layout = QHBoxLayout()
+        self.select_button = QPushButton("Select")
+        self.cancel_button = QPushButton("Cancel")
+
+        self.buttons_layout.addWidget(self.select_button)
+        self.buttons_layout.addWidget(self.cancel_button)
+
+        self.layout.addLayout(self.form_layout)
+        self.layout.addLayout(self.buttons_layout)
+
+        self.select_button.clicked.connect(self.get_option)
+        self.cancel_button.clicked.connect(self.reject)
+
+    def get_option(self):
+        if self.input_1.isChecked():
+            return True
+        elif self.input_2.isChecked():
+            return False
+        else:
+            self.reject()
+
+    def parse_input(self):
+        result = self.exec_()
+        if result == QDialog.Accepted:
+            return self.result()
+
+        return None
